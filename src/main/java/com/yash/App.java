262ba39216b3204d9fcba24a198f6b8751e45527
Yash Sahay <yashsahay999@gmail.com>
2025-08-23 17:25:44 +0530

    Created Student Bean using ClassPathXmlApplicationContext

--- src/main/java/com/yash/App.java
+++ src/main/java/com/yash/App.java
@@ -1,13 +1,14 @@
 package com.yash;
 
-/**
- * Hello world!
- *
- */
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+        Student s1 = (Student) context.getBean("student1");
+        System.out.println(s1);
     }
 }
---
+++ src/main/java/com/yash/Student.java
@@ -0,0 +1,28 @@
+package com.yash;
+
+public class Student {
+    private int id;
+    private String name;
+    private String address;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+}
